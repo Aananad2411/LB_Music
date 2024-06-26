31c0ac5cdacec5275ded5cbf446034b1a1a08dac
Aananad2411 <aanandsahu127@gmail.com>
2024-06-26 12:45:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -46,8 +46,8 @@ AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
 
 
 # Get this credentials from https://developer.spotify.com/dashboard
-SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", None)
-SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", None)
+SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", cd6b1fc089cd4db3a97e8ce93cb746bd)
+SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", ce9b560fe9a64e63b571e2f65b393ced)
 
 
 # Maximum limit for fetching playlist's track from youtube, spotify, apple links.
